246ccc234659a74f7e27aa0d521f0a2468b30c53
NorbertSychta <66336980+NorbertSychta@users.noreply.github.com>
2025-10-28 13:19:39 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('homeBtn').addEventListener('click', () => {
-    document.getElementById('content').innerHTML = '<p>This is the home page content.</p>';
-});
-
-document.getElementById('aboutBtn').addEventListener('click', () => {
-    document.getElementById('content').innerHTML = '<p>This is the about page content.</p>';
-});
